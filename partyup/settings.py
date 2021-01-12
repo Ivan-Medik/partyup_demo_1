4b0045c9ef45b867f72de4d28787f4bf5349dda3
Ivan-Medik <58795580+Ivan-Medik@users.noreply.github.com>
2021-01-12 23:46:00 +0300

    Update settings.py

--- partyup/settings.py
+++ partyup/settings.py
@@ -135,5 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+MEDIA_ROOT = Path(BASE_DIR / 'media')
